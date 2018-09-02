bd74c2894b3a5366c73d7ecc9493de2221bc825f
Hermit <yzy627@126.com>
2018-09-02 10:54:39 +0800

    测试

--- web/src/app/config.ts
+++ web/src/app/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-  static base_url = 'http://localhost:8080';  // 居然要加http!!!不然不会算作http请求，不能跨域
+  static base_url = '';  // 居然要加http!!!不然不会算作http请求，不能跨域
   static type_title: number = 1;
   static type_author: number = 2;
 
